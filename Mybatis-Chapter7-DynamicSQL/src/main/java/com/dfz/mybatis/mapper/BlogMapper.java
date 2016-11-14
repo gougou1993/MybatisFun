0779c75e2eef82fbfff3675e4fe1eb4346f17ed8
EdwardEricZhang <787814104@qq.com>
2016-11-14 14:58:01 +0800

    添加Chapter8

--- Mybatis-Chapter7-DynamicSQL/src/main/java/com/dfz/mybatis/mapper/BlogMapper.java
+++ Mybatis-Chapter7-DynamicSQL/src/main/java/com/dfz/mybatis/mapper/BlogMapper.java
@@ -2,6 +2,8 @@ package com.dfz.mybatis.mapper;
 
 import com.dfz.mybatis.model.Blog;
 
+import java.util.List;
+
 /**
  * Created by zhangsiyuan on 2016/11/14.
  */
@@ -11,5 +13,9 @@ public interface BlogMapper {
 
     public Blog blogChooseTest(Blog blog);
 
-    public Blog blogTrimTest(Blog blog);
+    public List<Blog> blogTrimTest(Blog blog);
+
+    public Blog blogWhereTest(Blog blog);
+
+    public List<Blog> blogForeachTest(List<Integer> ids);
 }
--- Mybatis-Chapter7-DynamicSQL/src/test/java/com/dfz/mybatis/test/Chapter7DynamicSQL.java
+++ Mybatis-Chapter7-DynamicSQL/src/test/java/com/dfz/mybatis/test/Chapter7DynamicSQL.java
@@ -11,6 +11,8 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zhangsiyuan on 2016/11/14.
@@ -62,12 +64,36 @@ public class Chapter7DynamicSQL {
     @Test
     public void testTrim() {
         Blog blog = new Blog();
-        System.out.println("------------测试otherwise------------");
+        System.out.println("------------测试trim------------");
 //        blog.setContent("content2");
         blog.setOwner("owner3");
         SqlSession session = sqlSessionFactory.openSession();
         BlogMapper mapper = session.getMapper(BlogMapper.class);
         System.out.println(mapper.blogTrimTest(blog));
+        session.close();
+    }
+
+    @Test
+    public void testWhere() {
+        Blog blog = new Blog();
+        System.out.println("------------测试where------------");
+        blog.setContent("content3");
+        blog.setOwner("owner3");
+        SqlSession session = sqlSessionFactory.openSession();
+        BlogMapper mapper = session.getMapper(BlogMapper.class);
+        System.out.println(mapper.blogWhereTest(blog));
+        session.close();
+    }
+
+    @Test
+    public void testForeach() {
+        System.out.println("------------测试foreach------------");
+        List<Integer> ids = new ArrayList<Integer>();
+        ids.add(1);
+        ids.add(2);
+        SqlSession session = sqlSessionFactory.openSession();
+        BlogMapper mapper = session.getMapper(BlogMapper.class);
+        System.out.println(mapper.blogForeachTest(ids));
         session.close();
     }
 }
